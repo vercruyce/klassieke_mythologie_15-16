719a4b70b867bc185e2f135432e8645c6e7612b0
bnavercr <brecht.vercruyce@ugent.be>
2016-05-24 18:12:26 +0200

    PDF build

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pandoc -s -S --toc -c "../resources/pandoc.css" -o "build/index.html" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
+echo "Building, can take a while..."
+pandoc -s -S --toc -c "../resources/pandoc.css" -o "build/notas.html" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
+pandoc -s -S --toc --include-in-header="resources/latex_headers.txt" -o "build/notas.pdf" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
