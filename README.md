8a67adf03cbd21d7c616d428869487906b864d2b
bnavercr <brecht.vercruyce@ugent.be>
2016-05-24 19:52:04 +0200

    kleine aanpassingen

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 Nota's voor het vak [klassieke mythologie 15-16](http://studiegids.ugent.be/2016/NL/studiefiches/A000619.pdf) gegeven aan UGent, schooljaar 2015-16.
 
+Originele auteurs:
+
+* Jonathan Deboosere
+* Robin De Pril
+* Jonas Van Wilder
+* Brecht Vercruyce
+
 Al deze nota's zijn **open source**. Voor aanvullingen of opmerkingen: zie [onze GitHub](https://github.com/marcarrelus/klassieke_mythologie_15-16)
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Building, can take a while..."
 pandoc -s -S --toc -c "../resources/pandoc.css" -o "build/notas.html" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
-pandoc -s -S --toc -V "linkcolor: blue" --include-in-header="resources/latex_headers.txt" -o "build/notas.pdf" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
+pandoc -s -S --toc -V "title:Cursusnota's \\\\ Klassieke mythologie 2015-2016" -V "date:\today" -V "linkcolor: blue" --include-in-header="resources/latex_headers.txt" -o "build/notas.pdf" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
