644f4254f3cb19db447c10764a718e0535d50a11
bnavercr <brecht.vercruyce@ugent.be>
2016-05-24 14:58:48 +0200

    relatieve padnaam voor css

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rootdir=$(pwd)
-pandoc -s -S --toc -c "$rootdir/resources/pandoc.css" -o "$rootdir/build/index.html" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
+pandoc -s -S --toc -c "../resources/pandoc.css" -o "build/index.html" README.md LICENSE.md Overzicht\ besproken\ mythes.md les*.md
